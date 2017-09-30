ede7918478f61ca58af7bb9b24bad892f607cf35
fedjensa <jens.hedberg@medieinstitutet.se>
2017-09-30 23:27:16 +0200

    vue cont

--- vue/app.js
+++ vue/app.js
@@ -1,7 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      form: {
+          new_message: ''
+      },
+      google:   'https://google.com'
+    },
+    methods: {
+        // updateMessage: function($event){
+        //     $event.preventDefault();
+
+        //     this.message = this.form.new_message;
+        //     this.form.new_message = '';
+        // }
+        updateMessage: function(){
+            this.message = this.form.new_message;
+            this.form.new_message = '';
+        }
     }
 });
 
